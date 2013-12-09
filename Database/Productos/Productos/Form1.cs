b1a318fe6d9bc60aa43344e9b6e6f07a2ec1eec0
Luis Vizcarra <l.vizcarra.delgado@isur.edu.pe>
2013-12-09 14:32:58 -0500

    linq

--- Database/Productos/Productos/Form1.cs
+++ Database/Productos/Productos/Form1.cs
@@ -17,6 +17,7 @@ namespace Productos
         //int ProductID, SupplierID, CategoryID, UnitsInStock, UnitsOnOrder, RecordLevel;
         //decimal UnitPrice;
         //bool Discontinued;
+        NorthwindDataContext db = new NorthwindDataContext();
         SqlConnection cn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Northwind;Data Source=.");
         public Form1()
         {
@@ -25,19 +26,17 @@ namespace Productos
         
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            //SqlCommand cmd = new SqlCommand("selectP", cn);
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);            
+            //DataSet ds = new DataSet();
+            //da.Fill(ds);
+            //dataGridView1.DataSource = ds.Tables[0];
+            var query = from p in db.Products
+                        select p;
+            dataGridView1.DataSource = query;
+                      
 
-
-            SqlCommand cmd = new SqlCommand("selectP", cn);
-            cmd.CommandType = CommandType.StoredProcedure;
-
-
-
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            
-            DataSet ds = new DataSet();
-            da.Fill(ds);
-            dataGridView1.DataSource = ds.Tables[0];
 
                   }
 
@@ -48,42 +47,66 @@ namespace Productos
 
         private void button5_Click(object sender, EventArgs e)
         {
-
-            int i = dataGridView1.CurrentRow.Index;
             
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            SqlCommand cmd = new SqlCommand("InsertP", cn);
-            cmd.CommandType = CommandType.StoredProcedure;
+            //SqlCommand cmd = new SqlCommand("InsertP", cn);
+            //cmd.CommandType = CommandType.StoredProcedure;
             
-            cmd.Parameters.Add(new SqlParameter("@ProductName",textBox2.Text));
-            cmd.Parameters.Add(new SqlParameter("@SupplierID",textBox3.Text));
-            cmd.Parameters.Add(new SqlParameter("@CategoryID",textBox4.Text));
-            cmd.Parameters.Add(new SqlParameter("@QuantityPerUnit",textBox5.Text));
-            cmd.Parameters.Add(new SqlParameter("@UnitPrice",textBox6.Text));
-            cmd.Parameters.Add(new SqlParameter("@UnitsInStock",textBox7.Text));
-            cmd.Parameters.Add(new SqlParameter("@UnitsOnOrder",textBox8.Text));
-            cmd.Parameters.Add(new SqlParameter("@ReorderLevel",textBox9.Text));
-            cmd.Parameters.Add(new SqlParameter("@Discontinued", textBox10.Text));
-            cn.Open();
-            cmd.ExecuteNonQuery();
-            cn.Close();
+            //cmd.Parameters.Add(new SqlParameter("@ProductName",textBox2.Text));
+            //cmd.Parameters.Add(new SqlParameter("@SupplierID",textBox3.Text));
+            //cmd.Parameters.Add(new SqlParameter("@CategoryID",textBox4.Text));
+            //cmd.Parameters.Add(new SqlParameter("@QuantityPerUnit",textBox5.Text));
+            //cmd.Parameters.Add(new SqlParameter("@UnitPrice",textBox6.Text));
+            //cmd.Parameters.Add(new SqlParameter("@UnitsInStock",textBox7.Text));
+            //cmd.Parameters.Add(new SqlParameter("@UnitsOnOrder",textBox8.Text));
+            //cmd.Parameters.Add(new SqlParameter("@ReorderLevel",textBox9.Text));
+            //cmd.Parameters.Add(new SqlParameter("@Discontinued", textBox10.Text));
+            //cn.Open();
+            //cmd.ExecuteNonQuery();
+            //cn.Close();
+
+            Product p = new Product();
+
+            p.ProductName = textBox2.Text;
+            p.SupplierID = int.Parse(textBox3.Text);
+            p.CategoryID = int.Parse(textBox4.Text);
+            p.QuantityPerUnit = textBox5.Text;
+            p.UnitPrice = decimal.Parse(textBox6.Text);
+            p.UnitsInStock = short.Parse(textBox7.Text);
+            p.UnitsOnOrder = short.Parse(textBox8.Text);
+            p.ReorderLevel = short.Parse(textBox9.Text);
+            p.Discontinued = bool.Parse(textBox10.Text);
+            db.Products.InsertOnSubmit(p);
+            db.SubmitChanges();
+
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            SqlCommand cmd = new SqlCommand("DeleteP", cn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add(new SqlParameter("@ProductID", textBox1.Text));
-            cn.Open();
-            cmd.ExecuteNonQuery();
-            cn.Close();
+            //SqlCommand cmd = new SqlCommand("DeleteP", cn);
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.Add(new SqlParameter("@ProductID", textBox1.Text));
+            //cn.Open();
+            //cmd.ExecuteNonQuery();
+            //cn.Close();
+
+            var query = from p in db.Products
+                        where p.ProductID == int.Parse(textBox1.Text)
+                        select p;
+            foreach (Product p in query)
+            {
+                db.Products.DeleteOnSubmit(p);
+            }
+            db.SubmitChanges();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            /*
             SqlCommand cmd = new SqlCommand("UpdateP", cn);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add(new SqlParameter("@ProductID", textBox1.Text));
@@ -98,7 +121,26 @@ namespace Productos
             cmd.Parameters.Add(new SqlParameter("@Discontinued", textBox10.Text));
             cn.Open();
             cmd.ExecuteNonQuery();
-            cn.Close();
+            cn.Close();*/
+            var query = from p in db.Products
+                        where p.ProductID == int.Parse(textBox1.Text)
+                        select p;
+            foreach (Product p in query)
+            {
+                p.ProductID = int.Parse(textBox1.Text);
+                p.ProductName = textBox2.Text;
+                p.SupplierID = int.Parse(textBox3.Text);
+                p.CategoryID = int.Parse(textBox4.Text);
+                p.QuantityPerUnit = textBox5.Text;
+                p.UnitPrice = decimal.Parse(textBox6.Text);
+                p.UnitsInStock =short.Parse(textBox7.Text);
+                p.UnitsOnOrder = short.Parse(textBox8.Text);
+                p.ReorderLevel = short.Parse(textBox9.Text);
+                p.Discontinued = bool.Parse(textBox10.Text);
+               
+            }
+            
+            db.SubmitChanges();
         }
     }
 }
--- Database/Productos/Productos/Properties/Settings.Designer.cs
+++ Database/Productos/Productos/Properties/Settings.Designer.cs
@@ -8,22 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Productos.Properties
-{
-
-
+namespace Productos.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=Northwind;Integrated Security=True")]
+        public string NorthwindConnectionString {
+            get {
+                return ((string)(this["NorthwindConnectionString"]));
             }
         }
     }
